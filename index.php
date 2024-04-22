f4ed115d2fb4b1bc98d8ed8fdee5f5e0e2bdf48b
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-22 20:55:03 +0500

    latest

--- index.php
+++ index.php
@@ -18,9 +18,6 @@ if (isset($_SESSION["userName"]) && isset($_SESSION["phone"])) {
         <div class="chat">
             <h2>Welcome to <span><?= $_SESSION["userName"] ?></span></h2>
             <div class="msg">
-
-
-
             </div>
             <div class="input_msg">
                 <input type="text" placeholder="Write msg Here" id="input_msg">
