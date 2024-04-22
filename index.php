cc6abbed26b56e767d6cb47c04dc4c9c2f4aba94
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-22 20:11:31 +0500

    latest

--- index.php
+++ index.php
@@ -2,25 +2,22 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ChatRoom</title>
-  <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ChatRoom</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-  <h1>ChatRoom</h1>
-  <div class="chat">
-    <h2>Welcome User</span></h2>
-    <div class="msg">
-      
-
-
+    <h1>ChatRoom</h1>
+    <div class="chat">
+        <h2>Welcome User</span></h2>
+        <div class="msg">
+        </div>
+        <div class="input_msg">
+            <input type="text" placeholder="Write msg Here" id="input_msg">
+            <button onclick="update()">Send</button>
+        </div>
     </div>
-    <div class="input_msg">
-      <input type="text" placeholder="Write msg Here" id="input_msg">
-      <button onclick="update()">Send</button>
-    </div>
-  </div>
 </body>
 <script src="js/script.js"></script>
